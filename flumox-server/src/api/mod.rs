47d80ff92559a0d7814fcbba8a7ed7f0b5b08180
Benjamin Swart <Benjaminswart@email.cz>
2023-06-18 20:17:59 +0200

    Add caching and security headers for API

--- flumox-server/src/api/mod.rs
+++ flumox-server/src/api/mod.rs
@@ -1,5 +1,10 @@
 mod session;
 mod view;
 
+use http::StatusCode;
 pub use session::{login, me};
 pub use view::view;
+
+pub async fn not_found() -> StatusCode {
+    StatusCode::NOT_FOUND
+}
--- flumox-server/src/main.rs
+++ flumox-server/src/main.rs
@@ -6,16 +6,24 @@ use axum::{
     Router, Server,
 };
 use deadpool_postgres::{Manager, Pool};
+use http::{
+    header::{
+        CACHE_CONTROL, CONTENT_SECURITY_POLICY, REFERRER_POLICY, X_CONTENT_TYPE_OPTIONS,
+        X_FRAME_OPTIONS,
+    },
+    HeaderValue,
+};
 use tokio_postgres::{Config, NoTls};
 use tower_http::{
-    compression::CompressionLayer, sensitive_headers::SetSensitiveHeadersLayer, trace::TraceLayer,
+    compression::CompressionLayer, sensitive_headers::SetSensitiveHeadersLayer,
+    set_header::SetResponseHeaderLayer, trace::TraceLayer,
 };
 use tracing::info;
 use tracing_subscriber::{
     filter::LevelFilter, fmt, layer::SubscriberExt, registry, util::SubscriberInitExt, Layer,
 };
 
-use crate::session::X_AUTH_TOKEN;
+use crate::{api::not_found, session::X_AUTH_TOKEN};
 
 mod api;
 mod db;
@@ -29,14 +37,36 @@ async fn serve(db: Pool) -> Result<()> {
         .route("/login", post(api::login))
         .route("/me", get(api::me))
         .route("/view", get(api::view))
+        .fallback(not_found)
+        .layer(SetResponseHeaderLayer::if_not_present(
+            CACHE_CONTROL,
+            HeaderValue::from_static("no-cache"),
+        ))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            CONTENT_SECURITY_POLICY,
+            HeaderValue::from_static("default-src 'none'"),
+        ))
+        .with_state(db);
+
+    let app = Router::new()
+        .nest("/api/", api)
         .layer(CompressionLayer::new().deflate(true).gzip(true).br(true))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            X_FRAME_OPTIONS,
+            HeaderValue::from_static("DENY"),
+        ))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            X_CONTENT_TYPE_OPTIONS,
+            HeaderValue::from_static("nosniff"),
+        ))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            REFERRER_POLICY,
+            HeaderValue::from_static("strict-origin-when-cross-origin"),
+        ))
         .layer(TraceLayer::new_for_http())
         .layer(SetSensitiveHeadersLayer::new(iter::once(
             X_AUTH_TOKEN.clone(),
-        )))
-        .with_state(db);
-
-    let app = Router::new().nest("/api", api);
+        )));
 
     let address = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 3000));
 
