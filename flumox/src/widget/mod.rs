25a4c28fadb0b2287c0d967a652f52817e7c44a7
Benjamin Swart <Benjaminswart@email.cz>
2023-06-30 14:36:51 +0200

    Define widgets with macro

--- flumox/src/widget/mod.rs
+++ flumox/src/widget/mod.rs
@@ -10,60 +10,82 @@ use crate::{
 
 mod prompt;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case", tag = "type")]
-pub enum Config {
-    Prompt(prompt::Config),
-}
+macro_rules! define_widgets {
+    ($($type: ident, $module: ident);*) => {
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        #[serde(rename_all = "kebab-case", tag = "type")]
+        pub enum Config {
+            $(
+                $type($module::Config),
+            )*
+        }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case", tag = "type")]
-pub enum State {
-    Prompt(prompt::State),
-}
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        #[serde(rename_all = "kebab-case", tag = "type")]
+        pub enum State {
+            $(
+                $type($module::State),
+            )*
+        }
 
-#[derive(Debug, Clone)]
-pub enum Instance {
-    Prompt(prompt::Config, prompt::State),
-}
+        #[derive(Debug, Clone)]
+        pub enum Instance {
+            $(
+                $type($module::Config, $module::State),
+            )*
+        }
 
-#[derive(Debug, Clone, Serialize, Fingerprint)]
-#[serde(rename_all = "kebab-case", tag = "type", content = "view")]
-pub enum View {
-    Prompt(prompt::View),
-}
+        #[derive(Debug, Clone, Serialize, Fingerprint)]
+        #[serde(rename_all = "kebab-case", tag = "type", content = "view")]
+        pub enum View {
+            $(
+                $type($module::View),
+            )*
+        }
 
-impl Config {
-    pub fn instance_default(self) -> Instance {
-        match self {
-            Config::Prompt(config) => {
-                let state = config.default_state();
-                Instance::Prompt(config, state)
+        impl Config {
+            pub fn instance_default(self) -> Instance {
+                match self {
+                    $(
+                        Config::$type(config) => {
+                            let state = config.default_state();
+                            Instance::$type(config, state)
+                        }
+                    )*
+                }
+            }
+
+            pub fn instance(self, state: State) -> Result<Instance, StateMismatchError> {
+                match (self, state) {
+                    $(
+                        (Config::$type(c), State::$type(s)) => Ok(Instance::$type(c, s)),
+                    )*
+                    #[allow(unreachable_patterns)]
+                    _ => Err(StateMismatchError),
+                }
             }
         }
-    }
 
-    pub fn instance(self, state: State) -> Result<Instance, StateMismatchError> {
-        match (self, state) {
-            (Config::Prompt(c), State::Prompt(s)) => Ok(Instance::Prompt(c, s)),
-            #[allow(unreachable_patterns)]
-            _ => Err(StateMismatchError),
+        impl Instance {
+            pub fn resolve(&self, path: &[&str], env: Environment) -> EvalResult {
+                match self {
+                    $(
+                        Instance::$type(c, s) => c.resolve(s, path, env),
+                    )*
+                }
+            }
+        
+            pub fn view(&self, ctx: ViewContext) -> ViewResult<View> {
+                let view = match self {
+                    $(
+                        Instance::$type(c, s) => c.view(s, ctx)?.map(View::$type),
+                    )*
+                };
+        
+                Ok(view)
+            }
         }
-    }
+    };
 }
 
-impl Instance {
-    pub fn resolve(&self, path: &[&str], env: Environment) -> EvalResult {
-        match self {
-            Instance::Prompt(c, s) => c.resolve(s, path, env),
-        }
-    }
-
-    pub fn view(&self, ctx: ViewContext) -> ViewResult<View> {
-        let view = match self {
-            Instance::Prompt(c, s) => c.view(s, ctx)?.map(View::Prompt),
-        };
-
-        Ok(view)
-    }
-}
+define_widgets!(Prompt, prompt);
