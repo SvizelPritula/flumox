05a4df3f4991373610d7967029f9a02ad099418e
Benjamin Swart <Benjaminswart@email.cz>
2023-07-01 23:48:58 +0200

    Add sample seed

---
+++ db/seed/sample-cs.sql
@@ -0,0 +1,134 @@
+INSERT INTO game (id, name)
+VALUES
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', 'Ukázková hra');
+
+INSERT INTO team (game, id, name, access_code, attributes)
+VALUES
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', '37f38c56-7bde-4fc5-b322-83eff8801dd8', 'Kyborgové', 'absurd-opportunity', '{}');
+
+INSERT INTO widget (game, id, ident, priority, config)
+VALUES
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', '3b284529-9164-42da-847d-c3fadf1d125a', 'intro', 0, '{
+        "type": "text",
+        "heading": "Něco ke hře",
+        "content": ["Cílem této hry je představit webovou aplikaci Flumox. Řešení všech \"šifer\" v této hře najdete v textu."],
+        "visible": "always"
+    }'),
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', '4bcd62b0-c2ed-46f2-b3c5-5a324573372c', 'to-start', 10, '{
+        "type": "prompt",
+        "name": "Cesta na start",
+        "details": [
+            "Takto vypadají šifry a navigace na stanoviště. Mohou obsahovat libovolný text, třeba popis cesty:",
+            "Jděte do kopce po silnici, dokud nenarazíte na velký dub."
+        ],
+        "prompt": "Jaký se zde nachází strom?",
+        "submit_button": "Odeslat",
+        "solutions": [
+            {"type": "alphanumeric", "solution": "dub"},
+            {"type": "alphanumeric", "solution": "velký dub"}
+        ],
+        "visible": "intro.visible",
+        "on_solution_correct": "Správně!",
+        "on_solution_incorrect": "Vaše odpověď je špatně. Přečtěte si text."
+    }'),
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', 'd2cbdb77-01bf-4c1a-80e1-0f9895de08f1', 'start', 20, '{
+        "type": "prompt",
+        "name": "První šifra",
+        "details": [
+            "Takto vypadá šifra s nápovědami. Pro nápovědy se dají nastavit flexibilní časové limity.",
+            "Zde se první dvě nápovědy odemknou chvíli po příchodu na šifru, třetí až po vyčerpání prvních dvou."
+        ],
+        "prompt": "Řešení:",
+        "submit_button": "Submit",
+        "solutions": [
+            {"type": "number", "solution": 2},
+            {"type": "alphanumeric", "solution": "dva"},
+            {"type": "alphanumeric", "solution": "dve"}
+        ],
+        "visible": "to-start.solved",
+        "on_solution_correct": "Správně!",
+        "on_solution_incorrect": "Vaše odpověď je špatně. Použijte nápovědy.",
+        "hints": [
+            {
+                "ident": "first",
+                "name": "První nápověda",
+                "content": ["Řešením je prvočíslo."],
+                "available": "this.visible + 10 s",
+                "visible": "always",
+                "take_button": "Odemknout",
+                "on_hint_taken": "Nápověda odemčena."
+            },
+            {
+                "ident": "second",
+                "name": "Druhá nápověda",
+                "content": ["Řešením je sudé číslo."],
+                "available": "this.visible + 20 s",
+                "visible": "always",
+                "take_button": "Odemknout",
+                "on_hint_taken": "Nápověda odemčena."
+            },
+            {
+                "ident": "third",
+                "name": "Třetí nápověda",
+                "content": ["Řešením je číslo 2."],
+                "available": "(this.hint.first.taken & this.hint.second.taken) + 10 s",
+                "visible": "always",
+                "take_button": "Odemknout",
+                "on_hint_taken": "Nápověda odemčena."
+            }
+        ]
+    }'),
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', '4e989707-f7e8-4c69-b189-95d9472036a1', 'answers', 30, '{
+        "type": "prompt",
+        "name": "Druhá šifra",
+        "details": [
+            "Šifry mohou mít více správných řešení. Při kontrole řešení se navíc ignoruje velikost písmen, diakritika, interpunkce a bílé znaky."
+        ],
+        "prompt": "Co se ignoruje při kontrole řešení?",
+        "submit_button": "Odeslat",
+        "solutions": [
+            {"type": "alphanumeric", "solution": "velikost písmen"},
+            {"type": "alphanumeric", "solution": "diakritika"},
+            {"type": "alphanumeric", "solution": "interpunkce"},
+            {"type": "alphanumeric", "solution": "bílé znaky"}
+        ],
+        "visible": "start.solved",
+        "on_solution_correct": "Správně!",
+        "on_solution_incorrect": "Špatné řešení. Přečtěte si text."
+    }'),
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', 'd72c008b-02fa-4a60-8b8b-9663d3ec124a', 'bonus', 40, '{
+        "type": "prompt",
+        "name": "Bonus",
+        "details": [
+            "Je také možné řešit více šifer zároveň. Tato šifra je navíc bonusová, takže jí není nutné vyřešit k postupu.",
+            "Nápověda k tomuto bonusu je nastavená tak, aby se odemkla po dokončení šifrovačky."
+        ],
+        "prompt": "Řešení:",
+        "submit_button": "Odeslat",
+        "solutions": [
+            {"type": "alphanumeric", "solution": "mlha"}
+        ],
+        "visible": "start.solved",
+        "on_solution_correct": "Správně!",
+        "on_solution_incorrect": "Špatné řešení.",
+        "hints": [
+            {
+                "ident": "main",
+                "name": "Nápověda",
+                "content": ["Řešením je \"mlha\"."],
+                "available": "answers.solved",
+                "visible": "always",
+                "take_button": "Odemknout",
+                "on_hint_taken": "Nápověda odemčena."
+            }
+        ]
+    }'),
+    ('f898f0d2-fb72-4046-a20b-7347a061b6a4', '97e528e5-09af-444a-a886-47a1b4183fb7', 'outro', 50, '{
+        "type": "text",
+        "heading": "Závěr",
+        "content": [
+            "Toto je konec této ukázky.",
+            "Mezi funkce neobsažené v této ukázce patří například odpočty a možnost upravování parametrů hry pro různé týmy."
+        ],
+        "visible": "answers.solved"
+    }');
